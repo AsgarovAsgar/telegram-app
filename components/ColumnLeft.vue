73847fe60cb500ab91685f1e9ce1031b83b4670a
Asgar Asgarov <asgarovasgar28@gmail.com>
2024-11-21 11:04:29 +0400

    add search input

--- components/ColumnLeft.vue
+++ components/ColumnLeft.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
 import { faker } from '@faker-js/faker';
+import { Input } from '@/components/ui/input'
 // create 10 dummy data for the chat,each person should be different
 const { data }  = useAsyncData('users', async () => {
   const res = await fetch('https://randomuser.me/api/?results=20')
@@ -19,8 +20,13 @@ const { data }  = useAsyncData('users', async () => {
       <button class="p-1.5 rounded-full grid place-content-center cursor-pointer hover:bg-gray-100 transition duration-200">
         <Icon name="ic:round-menu" class="size-5 bg-gray-500"/>
       </button>
-      <!-- <UInput icon="ic:round-search" variant="soft" :ui="{ base: 'rounded-full bg-gray-100' }" class="flex-1" placeholder="Search"></UInput> -->
-    </div>
+      <div class="relative flex w-full max-w-sm items-center">
+        <Input id="search" type="text" placeholder="Search" class="pl-10 border-2 focus-visible:ring-transparent bg-gray-200 rounded-full focus:border-blue-400 focus:bg-white" />
+        <span class="absolute start-3 inset-y-0 flex items-center justify-center">
+          <Icon name="ic:round-search" class="size-5 bg-gray-500"/>
+        </span>
+      </div>
+      </div>
 
     <div class="pl-3 py-1.5 flex flex-col overflow-y-scroll scrollbar">
       <div v-for="user in data" class="flex relative gap-1 p-1.5 rounded-lg items-center cursor-pointer hover:bg-gray-100">
